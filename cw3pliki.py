f82931b67bf5f07d1d21dc1eefdc634888be43d9
OliwierWilbrandt <161207691+OliwierWilbrandt@users.noreply.github.com>
2024-03-10 12:12:36 +0100

    dodanie cw3pliki

--- cw3pliki.py
+++ cw3pliki.py
@@ -1,9 +1,27 @@
 plik = open('tekst.txt', 'r', encoding='utf-8')
 znaki = plik.read(10)
 
-linia = plik.readline()
-linie = plik.readlines()
-plik.close()
-print(znaki)
-print(linia)
-print(linie)
+# linia = plik.readline()
+# linie = plik.readlines()
+# plik.close()
+# print(znaki)
+# print(linia)
+# print(linie)
+#
+# for l in linie:
+#     print(l)
+
+# plik = open('tekst.txt', 'a+')
+# plik.write('aaaaaaa')
+# plik.seek(105)
+# znaki = plik.read(10)
+# plik.close()
+# print(znaki)
+
+with open('tekst.txt', 'r') as f:
+    lines = f.readlines()
+print(lines)
+
+
+
+
