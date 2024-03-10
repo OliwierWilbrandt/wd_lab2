48f864d3c65687d64788ea792faaf236e290f331
OliwierWilbrandt <161207691+OliwierWilbrandt@users.noreply.github.com>
2024-03-10 12:41:13 +0100

    dodanie cw3zadania

---
+++ cw3zadania.py
@@ -0,0 +1,18 @@
+#Zad1
+# A = {1 - x for x in range(1, 11)}
+# B = {4 ** y for y in range(8)}
+# C = {x for x in B if x % 2 == 0}
+
+#Zad2
+# import random
+# l1 = [random.randint(1, 100) for x in range(10)]
+# print(l1)
+# l2 = [x for x in l1 if x % 2 == 0]
+# print(l2)
+
+#Zad3
+
+
+
+
+
