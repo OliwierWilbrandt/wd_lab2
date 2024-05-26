58a5f5e32cd2cd15b88028437a0a7a97927e05b6
OliwierWilbrandt <161207691+OliwierWilbrandt@users.noreply.github.com>
2024-05-26 15:15:17 +0200

    dodanie cw5pandas

--- cw5.py
+++ cw5.py
@@ -1,17 +1,90 @@
 import numpy as np
 
-a = np.array([20, 30, 40, 50])
-b = np.arrange(4)
-print(a)
-print(b)
+# a = np.array([20, 30, 40, 50])
+# b = np.arange(4)
+# print(a)
+# print(b)
+#
+# c = a - b
+# print(c)
+#
+# print(b**2)
+#
+# print(a)
+# a += b
+# print(a)
+# a -= b
+# print(a)
+#
+# d = a*b
+# print(d)
 
-c = a - b
-print(c)
 
-print(b**2)
+# b = np.arange(3)
+# print(b)
+# print(np.exp(b))
+# print(np.sqrt(b))
+# c = np.array([2., -1., 4.])
+# print(np.add(b,c))
 
-print(a)
-a += b
-print(a)
-a -= b
-print(a)
+# a = np.arange(3)
+# b = np.arange(2)
+# print(a)
+# print(b)
+# print(a.dot(b))
+# print(np.dot(a,b))
+# c = np.array([[1,5], [2,6], [7,4], [5,2]])
+# d = np.array([[2,5,4], [4,3,1]])
+# print(c)
+# print(d)
+# print(np.dot(c,d))
+# print(np.dot(d,c))
+
+
+# a = np.arange(12).reshape((3,4))
+# print(a)
+# print(a.sum())
+# print(a.sum(axis=0))
+# print(a.min(axis=1))
+# print(a.cumsum(axis=1))
+
+#
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# for b in a:
+#     for i in range(len(b)):
+#         print(b[i], end= '')
+#     # print("")
+#     print(b)
+#
+# a = np.arange(6).reshape((3,2))
+# print(a)
+# for b in a.flat:
+#     print(b)
+#     # print("")
+
+
+# a = np.arange(6)
+# print(a)
+# print(a.shape)
+# print("")
+# b = a.reshape((2,3))
+# print(b)
+# print(b.shape)
+# print("")
+# c = b.reshape((3,2))
+# print(c)
+# print(c.shape)
+# print("")
+# d = c.ravel()
+# print(d)
+# print(d.shape)
+# print("")
+# e = b.T
+# print(e)
+# print(e.shape)
+
+
+
+
+
