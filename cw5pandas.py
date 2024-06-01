a53cf7b26e71ae4db33855806ad075145658a1ae
OliwierWilbrandt <161207691+OliwierWilbrandt@users.noreply.github.com>
2024-06-01 11:42:43 +0200

    dodanie cw5pandas

--- cw5pandas.py
+++ cw5pandas.py
@@ -1,40 +1,83 @@
 import pandas as pd
 import numpy as np
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
-print(s)
+
+# s = pd.Series([1, 3, 5, np.nan, 6, 8])
+# print(s)
 s = pd.Series([10, 12, 8, 14], index=['a','b','c','d'])
 print(s)
 data = {'Kraj': ['Belgia','Indie','Brazylia'],
         'Stolica':['Bruksela','New Delhi','Brasilia'],
         'Populacja': [11190846,1303171035,207847528]}
 df = pd.DataFrame(data)
+# print(df)
+# print(df.dtypes)
+# # df = pd.read_csv('dane.csv', header=0, sep=";", decimal='.')
+# print(df)
+# df.to_csv('plik.csv', index=False)
+#
+# print(s['c'])
+# print(s.c)
+# print(df[0:1])
+# print("")
+# print(df['Populacja'])
+# print(df.iloc[0, 0])
+# print(df.loc[0, "Kraj"])
+# print(df.at[0, "Kraj"])
+#
+# ##Ćw6
+#
+# print('kraj:' + df.Kraj)
+# print(df.sample())
+#
+# print(df.sample(2))
+# print(df.sample(frac=0.5))
+# print(df.sample(n=10, replace=True))
+# print(df.head())
+# print(df.head(2))
+# print(df.tail(1))
+# print(df.describe())
+# print(df.T)
+
+# print(s[(s>9)])
+# print(s.where(s>10))
+# print(s.where(s>10, 'za duże'))
+# seria = s.copy()
+# seria.where(seria>10, 'za duże', inplace=True)
+# print("#######")
+# print(seria)
+#
+# print(s[~(s>10)])
+#
+# print(s[(s<13) & (s>8)])
+
+# print(df[(df.Populacja>1000000) & (df.index.isin([0,2]))])
+#
+# print('########')
+# szukaj=['Belgia', 'Brasilia']
+# print(df.isin(szukaj))
+
+s['e']=15
+print(s.e)
+s['f']=16
+print(s)
+
+df.loc[3]= 'dodane'
 print(df)
-print(df.dtypes)
-# df = pd.read_csv('dane.csv', header=0, sep=";", decimal='.')
+df.loc[4]=['Polska', 'Warszawa', 38675467]
 print(df)
-df.to_csv('plik.csv', index=False)
 
-print(s['c'])
-print(s.c)
-print(df[0:1])
-print("")
-print(df['Populacja'])
-print(df.iloc[0, 0])
-print(df.loc[0, "Kraj"])
-print(df.at[0, "Kraj"])
+new_df=df.drop([3])
+print(new_df)
 
-##Ćw6
+df.drop([3], inplace=True)
+print(df)
 
-print('kraj:' + df.Kraj)
-print(df.sample())
 
-print(df.sample(2))
-print(df.sample(frac=0.5))
-print(df.sample(n=10, replace=True))
-print(df.head())
-print(df.head(2))
-print(df.tail(1))
-print(df.describe())
-print(df.T)
+df['Kontynent']=['Europa','Azja',
+                 'Ameryka Południowa', 'Europa']
+
+print(df)
+
+
 
 
